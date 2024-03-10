fcd9068488a3588eb7d04490dc4b59373df3bda4
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-03-10 15:03:55 +0800

    Fix

--- grade.py
+++ grade.py
@@ -25,7 +25,7 @@ def test(i):
         if len(out) != 0:
             print(f'Test {i} failed')
             return 0
-        out = os.popen(f'echo \"select L_TAX from lineitem WHERE L_DISCOUNT > 0.02;\" | sqlite3 test.db').read()
+        out = os.popen(f'echo "select L_TAX from lineitem WHERE L_DISCOUNT > 0.02;" | sqlite3 test.db').read()
     if i in [1, 2, 3, 4, 5, 7]:
         expected = sorted(expected.splitlines())
         out = sorted(out.splitlines())
