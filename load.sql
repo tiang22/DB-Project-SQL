61fc1781b71eaee869dcbc2aa22f5b4d5802cac8
tiang22 <xinyangtian368@gmail.com>
2024-06-19 14:05:09 +0800

    update

---
+++ load.sql
@@ -0,0 +1,8 @@
+.import ./data/customer.tbl CUSTOMER
+.import ./data/lineitem.tbl LINEITEM
+.import ./data/nation.tbl NATION
+.import ./data/orders.tbl ORDERS
+.import ./data/part.tbl PART
+.import ./data/partsupp.tbl PARTSUPP
+.import ./data/region.tbl REGION
+.import ./data/supplier.tbl SUPPLIER
